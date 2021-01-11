a5c34d7e187013f90182073c78c7a083e8f9de8b
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-01-11 15:34:39 -0600

    Apply Unique IDs to Tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -28,6 +29,9 @@ var createTaskEl =function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
@@ -37,7 +41,10 @@ var createTaskEl =function(taskDataObj) {
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);    
-}
+    tasksToDoEl.appendChild(listItemEl); 
+    
+    // increase task counter for next unique id
+    taskIdCounter++;
+};
 
 formEl.addEventListener("submit", taskFormHandler);
