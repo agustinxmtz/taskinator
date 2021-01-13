c2d33766c5b83397376a2137eb2fa4d46df697b6
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-01-13 11:48:16 -0600

    Improving the UI by Adding New CSS to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,6 +188,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if(taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -207,8 +208,18 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
 };
 
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
+
+
+pageContentEl.addEventListener("dragleave" , dragLeaveHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
